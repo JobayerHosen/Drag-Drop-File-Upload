1dcd5b0938f800e71ec21e43b1557c144ae31ada
JobayerHosen <juba2732@gmail.com>
2021-08-27 10:35:38 +0600

    added features file upload  with xhr and file upload progress in the UI for individual file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const dropZone = document.getElementById("drop-zone");
 const fileInputField = document.getElementById("files-input");
 const uploadedFiles = document.getElementById("files");
+const trackUploadingFiles = {};
 
 const highlight = function () {
     dropZone.classList.add("highlight");
@@ -19,23 +20,69 @@ const browseHandler = function () {
     handleFiles(fileInputField.files);
 };
 
+/* HANDLES FILELIST GIVEN BY DROPHANDER AND BROWSEHANDER FUNCTION.
+ ADD FILES INFO TO TRACKUPLOADINGFILE OBJECT AND CALLS UPLOADFILE FUNCTION 
+ PASSING THE FILE AND FILE ID IN THE OBJECT*/
 const handleFiles = function (fileList) {
     const files = [...fileList];
     files.forEach((file) => {
-        createFileElement(file);
+        const fileId = createFileElement(file);
+        let fileElement = document.getElementById(fileId);
+        trackUploadingFiles[fileId] = {
+            fileElement: fileElement,
+            fileProgress: 0,
+            fileContent: file,
+            fileStatus: `<span class="file-status">Uploading</span>`,
+        };
+        uploadFile(file, fileId);
     });
 };
 
+/*UPLOADS FILE TO SERVER AND CALLS updateFileProgress FUNCTION 
+ON UPLOAD PROGRESSS AND ONLOAD */
+const uploadFile = function (file, fileId) {
+    const url = "";
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "maltipart/form-data");
 
+    xhr.upload.addEventListener("progress", (e) => {
+        let progress = Math.round((e.loaded / e.total) * 100);
+        trackUploadingFiles[fileId].fileProgress = progress;
+        updateFileProgress(fileId);
+    });
+    xhr.upload.addEventListener("load", (e) => {
+        trackUploadingFiles[fileId].fileStatus = `<span class="file-status completed">Completed</span>`;
+        updateFileProgress(fileId);
+    });
+    xhr.addEventListener("readystatechange", () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log("OK");
+        }
+    });
 
-// CREATE FILE ITEM ELEMENT TO ADD TO UPLOADED FILE SECTION
+    const formData = new FormData();
+    formData.append("file", file);
+    xhr.send(formData);
+};
+
+// UPADETE FILE PROGRESS AND STATUS TO CORRESPONDING FILE ITEM ELEMENT IN HTML
+const updateFileProgress = function (fileId) {
+    const fileElement = trackUploadingFiles[fileId].fileElement;
+    fileElement.querySelector("#file-progress").value = trackUploadingFiles[fileId].fileProgress;
+    const status = fileElement.querySelector("#file-status");
+    status.innerHTML = trackUploadingFiles[fileId].fileStatus;
+    status.classList.add("completed");
+};
+
+// CREATE FILE ITEM ELEMENT TO ADD TO UPLOADED FILE SECTION. ADD A ID TO IT. AND RETURN THE ID
 const createFileElement = function (file) {
+    const fileId = file.lastModified.toString();
     //FILE ELEMENT
     let fileElement = document.createElement("div");
     fileElement.className = "file";
-    fileElement.setAttribute("id", file.name);
+    fileElement.setAttribute("id", fileId);
 
-    console.log(file);
     fileElement.innerHTML = `
     <div class="thumb">
         <i class="far fa-file-image" aria-hidden="true"></i>
@@ -43,13 +90,14 @@ const createFileElement = function (file) {
     <div class="file-info">
         <div class="flex justify-between">
             <p class="file-name truncate"><span>${calculateFileSize(file.size)}</span> ${file.name} </p>
-            <p class="file-status">Uploading</p>
+            <p id="file-status"><span class="file-status">Uploading</span></p>
         </div>
         <progress id="file-progress" value="1" max="100">65%</progress>
     </div>
     `;
 
     uploadedFiles.appendChild(fileElement);
+    return fileId;
 };
 
 //CALCULATES FILE SIZE IN DIFFERRENT UNIT AND RETURN A FORMATED STRING.
